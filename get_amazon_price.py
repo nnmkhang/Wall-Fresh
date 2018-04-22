4b02747a9690e7e0ec222f4a8107ff2afd29b9cc
Khang Nguyen <nnmkhang166@gmail.com>
2018-04-22 18:55:28 -0400

    Amazon price checker
    Created a test file that gets the amazon price

--- get_amazon_price.py
+++ get_amazon_price.py
@@ -7,8 +7,8 @@ def getAmazonPrice(url):
     res = requests.get(url) # gets the url from the input and gets a request for it 
     res.raise_for_status() # checkpoint to make sure that the code is downloading the page correctly 
     soup = bs4.BeautifulSoup(res.text , "html.parser")
-    elems = soup.select('#buyNewSection > div > div > span > span')
-    print elems[0].text
+    elems = soup.select('#buyNewSection > div > div > span > span') # CSS selector by inspecting element and then right clicking to copy the path to the clipboard 
+    print elems[0].text # to get the text value of the thing we are pacing 
     
 url = raw_input("what product would you like to see the price of")
 print url
