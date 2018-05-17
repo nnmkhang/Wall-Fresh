27863043882a0baa731afb70a221a47467e48359
Khang Nguyen <nnmkhang166@gmail.com>
2018-05-17 14:29:44 -0400

    Script is almost fully working, making minor adjustments

--- Wall-Fresh.py
+++ Wall-Fresh.py
@@ -42,12 +42,42 @@ class sub:
 #print s.getTop()
 
 
-numOfSub = 5 # the number of subreddits
+def downloadUrl(url): #this function takes in a url string and downloads it to your pc
+    picUrl = url
+    picName = url.split("/")[-1]
+
+    if(picName.split(".")[-1] == "jpg" or picName.split(".")[-1] == "png"):
+        picName = directory+picName
+        print picName
+        urllib.urlretrieve(picUrl, picName)
+    else:
+        if(picUrl.split('/')[2] == "imgur.com"): # its a link to an non i.imgur link
+
+            if(picUrl.split('/')[-2] =="a"): # this indicates an imgur album
+                picName = "album error"
+                print picName
+
+            else:
+                
+                picUrl = "https://i.imgur.com/" + picName +".jpg"
+                picName = directory+picName+".jpg"
+                print picUrl 
+                urllib.urlretrieve(picUrl, picName)
+        else: # at this point we have hpyer linked stuff
+            print "text error" 
+#"C:\Users\nnmkh\Downloads\
+
+
+
+    #https://www.reddit.com/r/EarthPorn/comments/7eipbx/join_the_battle_for_net_neutrality_dont_let_the/
+
+
+directory = "C:\\Users\\nnmkh\\Desktop\\Wallpapers\\"
+numOfSub = 1 # the number of subreddits
 subreddits = []
 #subreddits[0]= sub("5","6",True)
 #print subreddits[0].getTop()
-for i in range(2):
-    print i 
+for i in range(numOfSub):
     #subreddits[i] = sub(tempName,tempNum,tempTop)
     tempName = str(raw_input("What subreddit do you want? (ex: earthporn) "))
     tempNum = int(raw_input("how many entries do you want? (ex: 50) "))
@@ -55,5 +85,21 @@ for i in range(2):
     temp = sub(tempName,tempNum,tempTop) 
     subreddits.append(temp)
 
-print subreddits[0].getName()
+
+
+reddit = praw.Reddit(client_id ="UB3VMr-CTHOaHQ",
+                     client_secret= "zPMe43C4JPelOMb_fwJYhG20mus",
+                     username ="Wall-Fresh",
+                     password = "123456",
+                     user_agent = "Wall-Fresh-Scraper")
+
+
+s = reddit.subreddit(subreddits[0].getName()).top(limit = subreddits[0].getPostNum())
+for submissions in s:
+    #print submissions.url
+    if (submissions.stickied!= True): # dosnt go through the sitcked content 
+        downloadUrl(submissions.url)
+    
+
+
 
